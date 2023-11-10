7a6093789066831ac7bdcd0ca6b234ac7b97b87c
Max Datsenko <datsenko.md@gmail.com>
2023-11-10 16:32:05 +0200

    add category change

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,37 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
+import cn from 'classnames';
 
 export default function Categories() {
+  const categories = [
+    { name: 'Все', id: 1 },
+    { name: 'Мясные', id: 2 },
+    { name: 'Вегетарианские', id: 3 },
+    { name: 'Гриль', id: 4 },
+    { name: 'Острые', id: 5 },
+    { name: 'Закрытые', id: 6 },
+  ];
+
+  const [activeId, setActiveId] = React.useState(categories[0].id);
+
+  const handleClick = (id) => () => setActiveId(id);
+
+  const renderItem = ({ name, id }) => (
+    <li
+      id={id}
+      key={id}
+      className={cn({ active: id === activeId })}
+      onClick={handleClick(id)}
+    >
+      {name}
+    </li>
+  );
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map(renderItem)}
       </ul>
     </div>
   );
