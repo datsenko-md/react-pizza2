e64db956317fa03eef8935a4f41d2774d9e2563b
Max Datsenko <datsenko.md@gmail.com>
2023-11-12 08:24:16 +0200

    add router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// eslint-disable-next-line no-unused-vars
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
