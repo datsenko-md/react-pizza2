039d3e10aac6a0cf82796363836556bdb60c50a8
Max Datsenko <datsenko.md@gmail.com>
2023-11-12 08:25:55 +0200

    add notFound page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import React from 'react';
 import './scss/app.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <div className="content">
         <div className="container">
           <Home />
+          {/* <NotFound /> */}
         </div>
       </div>
     </div>
